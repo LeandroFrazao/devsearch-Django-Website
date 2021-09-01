53c010b94e4cb6d0f2721a977731c09e91b15297
Leandro Frazao <developer.lrf@gmail.com>
2021-09-02 00:22:50 +0100

    First commit

---
+++ devsearch/urls.py
@@ -0,0 +1,9 @@
+from projects.views import projects
+from django.contrib import admin
+from django.urls import path , include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('projects.urls'))
+
+]
