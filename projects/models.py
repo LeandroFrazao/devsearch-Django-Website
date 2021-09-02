03412b65de4cf8e457a5b6a2f1bb74faee0159bc
Leandro Frazao <developer.lrf@gmail.com>
2021-09-02 17:29:15 +0100

    Building Models/Database

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True) # dont need to put quotes on 'Tag' if the Tag model is on top of the project model
+    vote_total = models.IntegerField(default=0,null= True, blank=True)
+    vote_ratio = models.IntegerField(default=0,null= True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+ 
+    def __str__(self):
+        return self.title
+
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up','Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    #owner =
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) #models.cascade(Delete all the reviews linked to the deleted project.  models.SET_NULL (it left alone the reviews if project is deleted)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
+from .models import Project
 # Create your views here.
 
 from django.http import HttpResponse
 
-projectsList = [
+""" projectsList = [
     {
         'id': '1',
         'title': 'Ecommerce Website',
@@ -20,23 +21,18 @@ projectsList = [
         'title': 'Social Network',
         'description': 'An open source project built by the community'
     }
-]
+] """
 
 
 def projects(request):
     #return HttpResponse("Here are our projects")
-
-    page = 'projects'
-    number = 10
-    context = {'page':page, 'number':number, 'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
     #return HttpResponse("Single Project" +" "+ str(pk))
 
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk) 
     context = {'project': projectObj}
     return render(request,'projects/single-project.html', context )
