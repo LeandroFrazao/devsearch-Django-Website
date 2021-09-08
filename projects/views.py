8aab58b76dd5a8cb71ccc12bb1391d04be04c4b5
Leandro Frazao <developer.lrf@gmail.com>
2021-09-08 14:30:57 +0100

    user login, logout and flash messages

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
 
@@ -17,6 +18,7 @@ def project(request, pk):
     context = {'project': projectObj}
     return render(request,'projects/single-project.html', context )
 
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
 
@@ -29,7 +31,7 @@ def createProject(request):
     context={'form':form}
     return render(request, 'projects/project_form.html', context)
 
-
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance= project)
@@ -43,6 +45,7 @@ def updateProject(request, pk):
     context={'form':form}
     return render(request, 'projects/project_form.html', context)
 
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
 
--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 
 urlpatterns =[
+
+    path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name='logout'),
+
+
+
     path('', views.profiles, name="profiles"),
-    path('profile/<str:pk>', views.userProfile, name='user-profile'),
+    path('profile/<str:pk>', views.userProfile, name="user-profile"),
 ]
