592982a918f4bf4011dd1dcda204d6682f41435c
Leandro Frazao <developer.lrf@gmail.com>
2021-09-08 12:58:32 +0100

    add signals

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 import uuid
 # Create your models here.
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True, blank=True)
@@ -23,7 +24,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str( self.user.username)
+        return str( self.username)
 
 
 class Skill(models.Model):
@@ -36,4 +37,3 @@ class Skill(models.Model):
     def __str__(self):
         return str( self.name)
 
- 
