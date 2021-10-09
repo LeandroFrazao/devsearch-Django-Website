9550f711f50c18613bc1b394715fb5779e87abba
Leandro Frazao <leandrofrazao@hotmail.com>
2021-10-09 03:51:32 +0100

    created api and frontend project page

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 from telnetlib import STATUS
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,11 +42,52 @@ INSTALLED_APPS = [
 
     'projects.apps.ProjectsConfig',
     'users.apps.UsersConfig',
+
+    'rest_framework',
+    'corsheaders',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(hours=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    
+ 
+    'corsheaders.middleware.CorsMiddleware',
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -121,6 +163,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
---
+++ frontend/main.js
@@ -0,0 +1,69 @@
+let projectsUrl = "http://127.0.0.1:8000/api/projects/";
+
+let getProjects = () => {
+  fetch(projectsUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      buildProjects(data);
+    });
+};
+
+let buildProjects = (projects) => {
+  let projectsWrapper = document.getElementById("projects--wrapper");
+  projectsWrapper.innerHTML = "";
+
+  for (let i = 0; projects.length > i; i++) {
+    let project = projects[i];
+    let projectCard = `
+    <div class="project--card">
+        <img src="http://127.0.0.1:8000${project.featured_image}"/>
+        <div>
+        <div class="card--header">
+            <h3> ${project.title}</h3>
+            <strong class="vote--option" data-vote="up" data-project="${
+              project.id
+            }">&#43</strong>
+            <strong class="vote--option" data-vote="down" data-project="${
+              project.id
+            }">&#8722</strong>
+        </div>
+        <i>${project.vote_ratio}% Positive feedback </i>
+        <p>${project.description.substring(0, 150)}</p>
+        </div>
+    </div>
+    `;
+    projectsWrapper.innerHTML += projectCard;
+  }
+
+  //add a listener
+  addVoteEvents();
+};
+
+let addVoteEvents = () => {
+  let voteBtns = document.getElementsByClassName("vote--option");
+  let token = localStorage.getItem("token");
+
+  for (let i = 0; voteBtns.length > i; i++) {
+    voteBtns[i].addEventListener("click", (e) => {
+      let vote = e.target.dataset.vote;
+      let project = e.target.dataset.project;
+
+      fetch(`http://127.0.0.1:8000/api/projects/${project}/vote/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ value: vote }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("success:", data);
+          getProjects();
+        });
+    });
+  }
+};
+
+getProjects();
