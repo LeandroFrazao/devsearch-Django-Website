fd038b1c8ae560077023e925db1380ed1891d6c1
Leandro Frazao <leandrofrazao@hotmail.com>
2021-10-20 15:46:37 +0100

    fix static dir

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -19,7 +19,8 @@ from dotenv import  load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print ("base_dir: ", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print ("base_dir: ", BASE_DIR)
+print ("base_dir2: ", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
